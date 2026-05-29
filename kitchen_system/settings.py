369df38bb42d3c9bb611acda27eaffb559f5faf9
Ra-cmd07 <ken0906914@gmail.com>
2026-05-29 13:07:56 +0800

    ok

--- kitchen_system/settings.py
+++ kitchen_system/settings.py
@@ -185,5 +185,4 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'cabalitrae6@gmail.com'
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
