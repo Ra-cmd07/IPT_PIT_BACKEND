381f17dcae6ab844fd07197d72b77dc8a58afb31
Ra-cmd07 <ken0906914@gmail.com>
2026-05-29 10:54:27 +0800

    ok

--- kitchen_system/urls.py
+++ kitchen_system/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from . import views
+import os
+
+def debug_env(request):
+    return JsonResponse({
+        'DJOSER_ACTIVATION_URL': os.environ.get('DJOSER_ACTIVATION_URL', 'NOT SET'),
+        'DJANGO_DEBUG': os.environ.get('DJANGO_DEBUG', 'NOT SET'),
+    })
 
 urlpatterns = [
     path('', views.api_root, name='api-root'),
+    path('debug-env/', debug_env),   # ← add this line
     path('admin/', admin.site.urls),
     path('api/', include('orders.urls')),
     path('api/auth/', include('users.urls')),
