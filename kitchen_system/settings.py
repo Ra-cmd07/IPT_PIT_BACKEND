d50cb38cf97e962cca75eb01be9e5cb3e1a5673f
Ra-cmd07 <ken0906914@gmail.com>
2026-05-29 12:52:25 +0800

    ok

--- kitchen_system/settings.py
+++ kitchen_system/settings.py
@@ -176,7 +176,12 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'HOKZDDJhRNF7cMaC_lgDSZFtEz8'),
 }
 
-EMAIL_BACKEND = 'users.resend_backend.ResendEmailBackend'
-DEFAULT_FROM_EMAIL = 'Kitchen System <onboarding@resend.dev>'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
